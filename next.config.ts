dc44cb0668ed87bdc0e4a742b98295d77c7e971a
Nondjock jean placide <abounidal494@gmail.com>
2026-01-28 13:41:02 +0100

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+
+const nextConfig = {
+  // ... autres configs
+  experimental: {
+    // Cela peut ne pas être disponible, donc à vérifier
+  },
+}
 
 export default nextConfig;
